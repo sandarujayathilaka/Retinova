dc391acc7f06e83da82b4acf863eb1c90a23a9cd
IT21181924 <IT21181924@my.sliit.lk>
2025-03-11 00:14:19 +0530

    modify the test record

--- web-app/src/pages/Nurse/View.jsx
+++ web-app/src/pages/Nurse/View.jsx
@@ -10,7 +10,8 @@ import { Separator } from "@/components/ui/separator";
 import {IdCard,IdCardIcon, UserCircle2Icon,UserCircle,UserCircle2,UserCircleIcon,CalendarIcon, User2, Loader2,Smartphone, Mail, Home, Circle, ArrowLeft, Pencil, Save, X } from "lucide-react";
 import MedicalHistory from "./MedicalHistory";
 import { Input } from "@/components/ui/input";
-import TreatmentPlans from "../TreatmentPlans";
+// import TreatmentPlans from "../TreatmentPlans";
+import TestRecords from "../testrecord/TestRecords"
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -412,7 +413,7 @@ const calculateAge = (birthDate) => {
 
           {activeTab === "treatment" && (
             <CardContent className="p-8 bg-white">
-              <TreatmentPlans />
+              <TestRecords patientId={patient.patientId}/>
             </CardContent>
           )}
         </Card>
